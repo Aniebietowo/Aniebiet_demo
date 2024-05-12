dafc2baa06dd3e002ca5afaa5926d63ea23701c8
Udynathcodes <uwahuduakobong@gmail.com>
2024-05-12 17:31:16 +0100

    my first commit

---
+++ Aniebiet_work/hello.c
@@ -0,0 +1,14 @@
+// Programming practical
+
+#include <stdio.h>
+
+ int main () 
+{ 
+	printf("hello world\n");
+	
+	printf("this is my first work on github\n");
+
+	printf("i hope i am welcome here.\n");
+
+		return 0;
+}
